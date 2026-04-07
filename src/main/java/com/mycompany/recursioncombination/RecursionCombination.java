22b64386d04ee6808cd72d80ef9b1c64714be29e
doomertheboomer <hackerdoomer@gmail.com>
2026-04-07 21:13:50 +0700

    maybe thats not what the prof wanted

--- src/main/java/com/mycompany/recursioncombination/RecursionCombination.java
+++ src/main/java/com/mycompany/recursioncombination/RecursionCombination.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.recursioncombination;
 
 /**
@@ -9,14 +8,15 @@ package com.mycompany.recursioncombination;
  * @author sph
  */
 public class RecursionCombination {
+
     private static long rand(long min, long max) {
-        return min + (long)(Math.random() * ((max - min) + 1));
+        return min + (long) (Math.random() * ((max - min) + 1));
     }
-    
+
     public static void main(String[] args) {
         Combination iter = new Iterative();
         Combination recu = new Recursive();
-        
+
         System.out.println("Testing function reliability: ");
         for (int i = 0; i < 10; i++) {
             long n = rand(4, 6);
@@ -30,7 +30,18 @@ public class RecursionCombination {
             }
         }
         System.out.println("Both implementations are working properly.");
-        
-        
+
+        System.out.println("Testing function limits: ");
+        long v = 0;
+        try {
+            while (true) {
+//                long iterres = iter.combination(v, v);
+                long recures = recu.combination(v, v);
+                System.out.println("Testing " + v + "C" + v);
+                v++;
+            }
+        } catch (Exception e) {
+            System.out.println("Exception " + e + " occured.");
+        }
     }
 }
